e0b5d5098537cbc775d1e4c6a853a30b54269790
ghubisz <gab.op.pl@op.pl>
2023-09-10 12:28:20 +0200

    during debugging of hamburger menu, it doesn't slide. added new component of menuitem

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="bg-gray-700 min-h-screen text-white">
       <HeaderNav />
+      <SideBar />
       <router-view />
   </div>
 </template>
 
 <script>
 import HeaderNav from './components/HeaderNav.vue';
+import SideBar from './components/SideBar.vue';
 
 export default {
     components: {
-      HeaderNav
+      HeaderNav,
+      SideBar
     }
 }
 </script>
